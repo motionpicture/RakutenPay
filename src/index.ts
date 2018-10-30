231af2b8d3873b062d7c0f0d2721b739a5ef0172
hayato.osada <hayato.osada@briswell.com>
2018-10-30 12:58:25 +0900

    MWSR-244 package修正

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,23 @@
  * @module
  */
 
- import * as factory from './factory';
+import * as factory from './factory';
+import { OrderStepBase } from './models/OrderStepBase';
+import { RakutenPayService } from './services/rakutenpay.service';
 
- export import factory = factory;
+export import factory = factory;
+
+export namespace service {
+    /**
+     * 楽天ペイサービス
+     */
+
+    export class RakutenPay extends RakutenPayService { }
+}
+
+export namespace model {
+    /**
+     * 注文ステップモデル
+     */
+    export class OrderStepBaseModel extends OrderStepBase {}
+}
